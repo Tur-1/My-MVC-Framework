e23c8fc9b80977757ada378440f6405628f0473b
TUR PC <alhrabi.tur1@gmail.com>
2024-03-15 07:01:39 +0300

    multiple authentication guards

--- app/views/pages/HomePage/index.php
+++
@@ -1,8 +0,0 @@
-<?php import('layouts.frontend.Header'); ?>
-<div class="row mb-3">
-    <div class="d-flex align-items-center justify-content-between">
-        <h1>Home </h1>
-
-    </div>
-</div>
-<?php import('layouts.frontend.Footer'); ?>
